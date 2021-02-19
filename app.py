a8da27d9ed5eaa0a10fb3e4b80620ecc464a85c2
Emily Chen <snoopy850321@gmail.com>
2021-02-19 12:12:44 -0800

    Morning call Twillio

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ with open("credentials.yaml") as f:
   creds = yaml.safe_load(f)
 
 client = Client(creds['account'], creds['token'])
-message = client.messages.create(to="+13108908971", from_="+16263228841",
-                                 body="Happy Birthday!")
-print(message.sid)
+
+def job():
+    call = client.calls.create(to="+13108908971",
+                               from_="+16263228841",
+                             url="http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient")
+
+schedule.every().day.at("8:30").do(job)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
